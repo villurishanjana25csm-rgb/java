54a36e521ff00cde09e88414c93c6ca97436ccdf
villurishanjana25csm-rgb <villurishanjana.25.csm@anits.edu.in>
2026-08-28 11:47:20 +0530

    Add ConDemoEx class with constructor and methods
    Implemented a constructor and methods to display student details.

---
+++ PracticePrograms/ConDemoEx.java
@@ -0,0 +1,30 @@
+package javacore;
+
+     public class ConDemoEx {
+	    // class or instance variables
+		int rollNo;
+	    String name;          
+	    float fee;
+		
+		// constructor
+		public ConDemoEx(int i,String n, float f) {
+		 this.rollNo = i;
+		 this.name= n;
+		 this.fee= f;
+		}
+		void sample(){
+			System.out.println("ROLL NO is:" + this.rollNo);
+			System.out.println("NAME is:" + this.name);
+			System.out.println("FEE is:" + this.fee);
+		}
+		void example() {
+			System.out.println("some other method named example");
+		}
+		
+		public static void main(String[] args) {
+			System.out.println("This is main method..!");
+			ConDemoEx cd = new ConDemoEx(235,"ravi", 4350.0f);
+			cd.sample();
+			cd.example();
+		}
+}
